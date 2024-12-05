a5f023fa8798e027834e201322d68bfc7287c0b2
QualityNinja <pavelbabok@gmail.com>
2024-12-05 21:51:59 +0300

    ДЗ № 15 (Задание 1)

---
+++ src/main/java/Homework_15/Task_1/Attributes.java
@@ -0,0 +1,13 @@
+package Homework_15.Task_1;
+
+import lombok.Data;
+
+
+@Data
+public class Attributes {
+    String name;
+    String status;
+    String image;
+    String link;
+    String brand;
+}
---
+++ src/main/java/Homework_15/Task_1/DataList.java
@@ -0,0 +1,11 @@
+package Homework_15.Task_1;
+
+import lombok.Data;
+
+@Data
+public class DataList {
+   String id;
+   String type;
+   String recommendationId;
+   Attributes attributes;
+}
